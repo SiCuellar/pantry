a829ff1565446126ef1345d24c3f2d2e98a7e3e5
SilvestreCuellar <cuellarsilvestre@gmail.com>
2018-07-25 14:53:25 -0600

    Silvestre Cuellar

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Pantry
+  attr_reader :stock
+
+
+  def initialize
+    @stock = {}
+
+  end
+
+  def stock_check(item)
+    if
+      @stock[item]
+    else
+      @stock.keys.include?(item) == false
+      @stock[item] = 0
+    end
+  end
+
+  def restock(item, amount)
+    # @stock.keys.include?(item) == false
+    #  @stock[item] = 0
+
+   new_total = @stock[item].value + amount
+   @stock[item] = new_total
+ end
 
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,30 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
 
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_it_can_have_stock
+    pantry = Pantry.new
+    expected = {}
+    assert_equal expected, pantry.stock
+  end
+
+  def test_it_can_check_stock
+    pantry = Pantry.new
+
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_it_can_restock
+
+    pantry = Pantry.new
+
+    pantry.restock("Cheese", 10)
+
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
 end
